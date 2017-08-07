95bc82e55783d0baad5c6691a388cdea776f7903
Aryan <aryan.stateofappiness@gmail.com>
2017-08-07 16:30:32 +0530

    testing emails

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -33,7 +33,89 @@ class RegisterController extends Controller
 
     public function createUser(Request $request) {
 
+        $client = SesClient::factory(array(
+                "endpoint" => 'http://email-smtp.us-west-2.amazonaws.com',
+                "accessKeyId" => env('SES_KEY', ''), 
+                "secretAccessKey"=> env('SES_SECRET', ''), 
+                  'version'=> 'latest',     
+                  'region' => env('SES_REGION', '')
+              ));
 
+              $mail = array();
+              $mail['Source'] = \Config::get('constants.email.FROM_ADDRESS');
+              $mail['Destination']['ToAddresses'] = [$request->email];
+              $mail['Message']['Subject']['Data'] = 'OTP verification for Advts';
+              $mail['Message']['Body']['Text']['Data'] = 'Your OTP for email verification is '. $randomNumber;
+              
+              try {
+                   $result = $client->sendEmail($mail);
+              } catch (SesException $e) {
+                    print_r('SES email: ');
+                    print_r($e->getMessage());
+                   //return response()->json(['error' => 'Error sending email. Please try again'], 500);
+              }
+
+$mail = new \PHPMailer;
+
+// Tell PHPMailer to use SMTP
+$mail->isSMTP();
+
+// If you're using Amazon SES in a Region other than US West (Oregon), 
+// replace email-smtp.us-west-2.amazonaws.com with the Amazon SES SMTP  
+// endpoint in the appropriate Region.
+$mail->Host = 'email-smtp.us-west-2.amazonaws.com';
+
+// Tells PHPMailer to use SMTP authentication
+$mail->SMTPAuth = true;
+
+// Replace smtp_username with your Amazon SES SMTP user name.
+$mail->Username = 'AKIAJRJ3BQCS2AK464UQ';
+
+// Replace smtp_password with your Amazon SES SMTP password.
+$mail->Password = 'a46dBNsiRNrAwL21VTCoxIYEU/TGIrJaeq0ovWuV';
+
+// Enable SSL encryption
+$mail->SMTPSecure = 'ssl';
+
+// The port you will connect to on the Amazon SES SMTP endpoint.
+$mail->Port = 465;
+
+// Replace sender@example.com with your "From" address. 
+// This address must be verified with Amazon SES.
+$mail->setFrom('info@wellbabys.com', 'Advtr');
+
+// Replace recipient@example.com with a "To" address. If your account 
+// is still in the sandbox, this address must be verified.
+// Also note that you can include several addAddress() lines to send
+// email to multiple recipients.
+$mail->addAddress('aryan.stateofappiness@gmail.com', 'Aryan');
+
+// Tells PHPMailer to send HTML-formatted email
+$mail->isHTML(true);
+
+// The subject line of the email
+$mail->Subject = 'Amazon SES test (SMTP interface accessed using PHP)';
+
+// The HTML-formatted body of the email
+$mail->Body    = '<h1>Email Test</h1>
+    <p>This email was sent through the 
+    <a href="http://aws.amazon.com/ses/">Amazon SES</a> SMTP
+    interface using the <a href="https://github.com/PHPMailer/PHPMailer">
+    PHPMailer</a> class.</p>';
+
+// The alternative email body; this is only displayed when a recipient
+// opens the email in a non-HTML email client. The \r\n represents a 
+// line break.
+$mail->AltBody = "Email Test\r\nThis email was sent through the 
+Amazon SES SMTP interface using the PHPMailer class.";
+
+if(!$mail->send()) {
+    echo 'Email not sent.';
+    echo 'Mailer Error: ' . $mail->ErrorInfo;
+} else {
+    echo 'Email sent!';
+}
+exit;
         $v = Validator::make($request->all(), [
             'email' => 'required|email',
             'password' => 'min:6',
