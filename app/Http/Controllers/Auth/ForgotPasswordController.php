6aa7cd2ec1dc817b6e22d7b3372680a9bf6dcc8a
Aryan <aryan.stateofappiness@gmail.com>
2017-08-10 23:47:25 +0530

    forgot password api

--- app/Http/Controllers/Auth/ForgotPasswordController.php
+++ app/Http/Controllers/Auth/ForgotPasswordController.php
@@ -4,6 +4,13 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\SendsPasswordResetEmails;
+use Illuminate\Http\Request;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Mail;
+use Aws\Ses\SesClient;
+use Aws\Ses\Exception\SesException;
+use Illuminate\Support\Facades\Validator;
+
 
 class ForgotPasswordController extends Controller
 {
@@ -28,5 +35,49 @@ class ForgotPasswordController extends Controller
     public function __construct()
     {
         $this->middleware('guest');
+    }
+
+    public function index(Request $request) {
+        
+        $v = Validator::make($request->all(), [
+            'email' => 'required|email'
+        ]);
+
+        if ($v->fails())
+        {
+             return response()->json(['error' => $v->errors()], 500);
+        }
+
+        try {
+            $user = \App\User::where('email', $request->email)
+                ->where('login_type', \Config::get('constants.loginTypes.EMAIL'))
+                ->first();
+            $password = str_random(8);    
+            $user->password = password_hash($password, PASSWORD_BCRYPT);
+            $user->save();    
+
+            $client = SesClient::factory(array(
+                "credentials" => ['key' => env('SES_KEY', ''), 'secret' => env('SES_SECRET', '')],
+                  'version'=> 'latest',
+                  'region' => env('SES_REGION', '')
+                ));
+
+                $mail = array();
+                $mail['Source'] = \Config::get('constants.email.FROM_ADDRESS');
+                $mail['Destination']['ToAddresses'] = [$request->email];
+                $mail['Message']['Subject']['Data'] = 'New password for Advts';
+                $mail['Message']['Body']['Text']['Data'] = 'Your new password for advtr is '.$password;
+
+                try {
+                     $result = $client->sendEmail($mail);
+                     return response()->json(['success' => 'Password sent successfully on your email'], 200);
+                } catch (SesException $e) {
+                      return response()->json(['error' => 'Email sending failed'], 500);
+                }
+
+        } catch (ModelNotFoundException $e) {
+            return response()->json(['error' => 'We are not able to find your email address'], 500);
+        }
+        
     }
 }
